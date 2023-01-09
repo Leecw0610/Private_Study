1f0335427ecc29597a3be37adcb9a4b6db7e81ac
Leecw0610 <118493679+Leecw0610@users.noreply.github.com>
2023-01-09 21:37:56 +0900

    Add files via upload

---
+++ SQLD/TEST.sql
@@ -0,0 +1,16 @@
+--SQL developer 의 주석
+
+--행 단위 주석 : ctrl + /
+
+--범위 주석 : alt + shift + c
+
+/*asdwgasgas
+asdgasgdg
+asdgasg
+*/
+
+-- tab이라는 테이블로부터 모든 행과 모든 칼럼을 다 가져와서 출력하라!
+
+-- 실행하는 단축키 : ctrl + enter
+
+select * from tab;
